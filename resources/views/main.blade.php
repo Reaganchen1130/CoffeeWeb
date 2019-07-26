f71e36a4751d3ee912f4e489a70968b4198cb584
korverchen <tobychen5487@gmail.com>
2019-07-26 19:10:36 +0800

    [2019/07/26] add navbar

---
+++ resources/views/main.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <style>
+        .cex {
+            color: #F2EEE5;
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #F2EEE5;
+        }
+
+        li {
+            float: right;
+        }
+
+        li a {
+            display: block;
+            color: #B76E6E;
+            text-align: center;
+            padding: 30px 16px;
+            text-decoration: none;
+            font-size: 30px;
+        }
+
+        li a:hover {
+            background-color: #F2EEE5;
+        }
+    </style>
+
+</head>
+
+
+
+<body>
+    <div class="cex">
+        <ul>
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+    </div>
+    <div class="cex">
+        <nav class="navbar navbar-light ">
+            <a class="navbar-brand" href="#">Navbar</a>
+        </nav>
+    </div>
+</body>
+
+</html>
